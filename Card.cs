6462f9d6e577021bdf81839a45f1c00c67bfe98f
PatrickCheng64 <patrick020805@yahoo.com.tw>
2021-12-15 18:05:18 +0800

    Card

---
+++ Card.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System;
+
+/*
+ * Card class, can be compared for sorting
+ * and have a tooltip message...
+ */ 
+public class Card : MonoBehaviour,IComparable {
+    
+    public string tooltipMessage = "自定義卡片名稱";
+
+    [HideInInspector]
+    public int id = 0;
+ 
+    // Compare based on card id
+    int IComparable.CompareTo(object obj)
+    {
+        Card otherCard = obj as Card;
+        if(otherCard != null)
+        {
+            return id.CompareTo(otherCard.id);
+        }
+        else
+        {
+            return 0;
+        }
+
+    }
+}
---
+++ Sample.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Sample : MonoBehaviour {
+
+    public CardDeckInterface cardDeck;
+
+    public Text selectedCardText;
+
+    private string sampleDescription;
+    	
+    void Start()
+    {
+
+        sampleDescription = selectedCardText.text;
+
+        /*
+         * 您可以使用 OnCardSelected 回調
+          * 如果您願意等待動畫完成
+          * 或 Update() 函數中在用戶單擊卡片後立即獲取索引的方式
+         */
+        cardDeck.OnCardSelected = OnCardChanged;
+    }
+
+    void OnCardChanged(int cardIndex)
+    {
+        Card currentCard = cardDeck.GetCurrentCard();
+
+        if (currentCard != null)
+            selectedCardText.text = sampleDescription + currentCard.tooltipMessage;
+    }
+
+	// Update is called once per frame
+	void Update () {
+
+        /*
+          *您可以使用 OnCardSelected 回調
+          * 如果您願意等待動畫完成
+          * 或下面的方式在用戶點擊卡片後立即獲取索引
+         */
+        //Card currentCard = cardDeck.GetCurrentCard();
+
+        //if (currentCard != null)
+        //    selectedCardText.text = sampleDescription + currentCard.tooltipMessage;
+
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            cardDeck.SetCurrentIndex(cardDeck.GetCurrentIndex() - 1);
+        }
+
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            cardDeck.SetCurrentIndex(cardDeck.GetCurrentIndex() + 1);
+        }
+    }
+}
